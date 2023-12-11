64b012dbf903fb40d1cff0fc7fe4709b23bab85d
myrg15 <merishield@gmail.com>
2023-12-11 18:27:23 +0100

    feat: variable environment configuration

--- src/database.ts
+++ src/database.ts
@@ -1,2 +1,19 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "mysql",
+    password: "123456789",
+    database:"appointment_scheduling", 
+    //entities: 
+    //migrations: 
+    synchronize: true,
+    logging: false,
+
+
+
+
+})
