29cc324b7fa92d497a1124d338372c4b0c65e210
myrg15 <merishield@gmail.com>
2023-12-13 22:44:25 +0100

    feat: delete mgartions

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,23 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+//migrations
+
+//models
+import { Users } from "./models/Users";
+import { Appointments } from "./models/Appointments";
+import { Reviews } from "./models/Reviews";
+import { Treatments } from "./models/Treatments";
+import { AppointmentTreatment } from "./models/AppointmentTreatment";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "mysql",
+    username: "root",
     password: "123456789",
     database:"appointment_scheduling", 
-    //entities: 
-    //migrations: 
+    entities:[Users, Appointments, Reviews, Treatments, AppointmentTreatment], 
+    migrations:[], 
     synchronize: true,
     logging: false,
 })
