3d7768e44a77befcbb092884e8aa3f49e12c7985
myrg15 <merishield@gmail.com>
2023-12-15 16:35:55 +0100

    feat: creation migration Appointments add database

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Users1702653336802 } from "./migration/1702653336802-Users";
+import { Appointments1702654139564 } from "./migration/1702654139564-Appointments";
 
 
 export const AppDataSource = new DataSource({
@@ -11,7 +12,7 @@ export const AppDataSource = new DataSource({
     password: "123456789",
     database:"appointment_scheduling", 
     entities:[], 
-    migrations:[Users1702653336802], 
+    migrations:[Users1702653336802, Appointments1702654139564], 
     synchronize: false,
     logging: false,
 })
