6edf8e57b13252061fd6c6eaada80e8e0f452ea9
myrg15 <merishield@gmail.com>
2023-12-18 22:32:58 +0100

    feat:insert migrations Treatments, Reviews

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,17 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
 import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
 import { Users } from "./models/Users";
-import { Appointments } from "./models/Appointments";
-import { Treatments } from "./models/Treatments";
 import { Reviews } from "./models/Reviews";
+import { Treatments } from "./models/Treatments";
+import { Appointments } from "./models/Appointments";
+
 import { Users1702930737798 } from "./migration/1702930737798-Users";
+import { Reviews1702934439449 } from "./migration/1702934439449-Reviews";
+import { Treatments1702932913324 } from "./migration/1702932913324-Treatments";
 import { Appointments1702931883584 } from "./migration/1702931883584-Appointments";
+
 dotenv.config({});
 
 export const AppDataSource = new DataSource({
@@ -18,7 +22,12 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD,
   database: "appointment_scheduling",
   entities: [Users, Appointments, Treatments, Reviews],
-  migrations: [Users1702930737798, Appointments1702931883584],
+  migrations: [
+    Users1702930737798,
+    Appointments1702931883584,
+    Treatments1702932913324,
+    Reviews1702934439449,
+  ],
   synchronize: false,
   logging: false,
 });
