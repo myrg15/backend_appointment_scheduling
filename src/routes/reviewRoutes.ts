86ce590d75bc925b8e9f8bd6555cd13e2d9b6385
myrg15 <merishield@gmail.com>
2023-12-26 16:16:09 +0100

    feat: add route review

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -1,4 +1,18 @@
-import { Router } from "express";
+import express from "express";
 import { auth } from "../middlewares/auth";
 
-import {}
+import {
+  getAllReviews,
+  createReview,
+  updateReview,
+  deleteReview,
+} from "../controllers/reviewControler";
+
+const route = express.Router();
+
+route.get("/all", getAllReviews);
+route.post("/create", createReview);
+route.put("/review_update/:id", updateReview);
+route.delete("/review_delete/:id", deleteReview);
+
+export { route as reviewRoutes };
