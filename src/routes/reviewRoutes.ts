9c0584a66789e1bc3681d58e284df06a3844e4a6
myrg15 <merishield@gmail.com>
2023-12-27 02:18:10 +0100

    feat: remove function create reviews, import in review router

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -3,7 +3,6 @@ import { auth } from "../middlewares/auth";
 
 import {
   getAllReviews,
-  createReview,
   updateReview,
   deleteReview,
 } from "../controllers/reviewControler";
@@ -11,7 +10,7 @@ import {
 const route = express.Router();
 
 route.get("/all", getAllReviews);
-route.post("/create", createReview);
+//route.post("/create", createReview);
 route.put("/review_update/:id", updateReview);
 route.delete("/review_delete/:id", deleteReview);
 
